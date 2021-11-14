a4de5800e21ae90a20f31b16029169404060bc2f
GregBiglione <gregory.biglione81@gmail.com>
2021-11-14 04:16:24 +0100

    Added fromDailyJson() & mapStringToWeatherCondition() in Weather

--- lib/model/weather_model.dart
+++ lib/model/weather_model.dart
@@ -6,6 +6,7 @@ enum WeatherCondition {
   atmosphere,
   clear,
   mist,
+  fog,
   lightCloud,
   heavyCloud,
   unknown,
@@ -14,11 +15,78 @@ enum WeatherCondition {
 class Weather {
   late String description;
   late double temperature;
-  late String fellLikeTemperature;
+  late String feelLikeTemperature;
   late int cloudiness;
   late WeatherCondition condition;
   late DateTime date;
 
-  Weather({required this.description, required this.temperature, required this.fellLikeTemperature,
+  Weather({required this.description, required this.temperature, required this.feelLikeTemperature,
     required this.cloudiness, required this.condition, required this.date});
+
+  //----------------------------------------------------------------------------
+  //----------------------- Create Weather from JSON ---------------------------
+  //----------------------------------------------------------------------------
+
+  Weather fromDailyJson(dynamic daily) {
+    var cloudiness = daily["clouds"];
+    var weather = daily["weather"][0];
+
+    return Weather(
+        description: weather["description"].toString(),
+        temperature: daily["temp"]["day"].toDouble(),
+        feelLikeTemperature: daily["feels_like"]["day"].toDouble(),
+        cloudiness: cloudiness,
+        condition: mapStringToWeatherCondition(weather["main"], cloudiness),
+        date: DateTime.fromMicrosecondsSinceEpoch(daily["dt"] * 1000, isUtc: true)
+    );
+  }
+
+  //----------------------------------------------------------------------------
+  //----------------------- Create map string weather condition ----------------
+  //----------------------------------------------------------------------------
+
+  WeatherCondition mapStringToWeatherCondition(String input, int cloudiness) {
+    WeatherCondition condition;
+
+    switch(input) {
+      case "Thunderstorm":
+        condition = WeatherCondition.thunderstorm;
+        break;
+      case "Drizzle":
+        condition = WeatherCondition.drizzle;
+        break;
+      case "Rain":
+        condition = WeatherCondition.rain;
+        break;
+      case "Snow":
+        condition = WeatherCondition.snow;
+        break;
+      case "Tornado":
+        condition = WeatherCondition.atmosphere;
+        break;
+      case "Clear":
+        condition = WeatherCondition.clear;
+        break;
+      case "Mist":
+        condition = WeatherCondition.mist;
+        break;
+      case "Fog":
+        condition = WeatherCondition.fog;
+        break;
+      case "Clouds":
+        condition = cloudiness >= 85
+        ? WeatherCondition.heavyCloud
+        : WeatherCondition.lightCloud;
+        break;
+      case 'Smoke':
+      case 'Haze':
+      case 'Dust':
+      case 'Sand':
+      case 'Ash':
+      case 'Squall':
+      default:
+        condition = WeatherCondition.unknown;
+    }
+    return condition;
+  }
 }
